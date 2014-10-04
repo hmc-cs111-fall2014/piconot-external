b1488d6b64b586bbec5bc67729cee4ae356affdf
Sisi Cheng <scheng@hmc.edu>
2014-10-04 14:35:45 -0700

    Finished abstract syntax.

--- src/main/scala/dsl/picobot/ir/AST.scala
+++ src/main/scala/dsl/picobot/ir/AST.scala
@@ -3,4 +3,26 @@ package dsl.picobot.ir
 sealed abstract class AST
 sealed abstract class File extends AST
 
+case class Equal(lhs: lhs, rhs: rhs) extends File
+case class lhs(state: State, surrounding: Rest) extends File
+case class rhs(state: State, nextDir: Dir) extends File
+
+class Rest extends File
+case class Plus(dir: Dir, rest: Rest) extends Rest
+case class Minus(dir: Dir, rest: Rest) extends Rest
+case class Mult(dir: Dir, rest: Rest) extends Rest
+
 case class State(n: Int) extends File
+
+class Surrounding extends File
+case class Blocked() extends Surrounding
+case class Open() extends Surrounding
+case class Any() extends Surrounding
+
+class Dir() extends File
+case class N() extends Dir
+case class E() extends Dir
+case class W() extends Dir
+case class S() extends Dir
+case class Stay() extends Dir
+
