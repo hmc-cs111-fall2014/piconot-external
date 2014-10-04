d9c9d1300d9e1b52ad25be3314d59c6557f09eaa
Nick Carter <njc093@gmail.com>
2014-10-04 14:05:04 -0700

    main function can read in (and print out) a file

--- src/main/scala/dsl/picobot/bot.scala
+++ src/main/scala/dsl/picobot/bot.scala
@@ -1,16 +1,26 @@
-package dsl.picobot
+//package dsl.picobot
 
-import scala.tools.nsc.EvalLoop
-import dsl.picobot.parser.PicoParser
-import dsl.picobot.semantics.eval
+//import scala.tools.nsc.EvalLoop
+//import dsl.picobot.parser.PicoParser
+//import dsl.picobot.semantics.eval
 
-object bot extends EvalLoop with App {
-  override def prompt = "> "
-
-  loop { line =>
-    PicoParser(line) match {
-      case PicoParser.Success(t, _) => println(eval(t))
-      case e: PicoParser.NoSuccess => println(e)
+//object bot extends EvalLoop with App {
+object bot extends App {
+  override def main(args: Array[String]): Unit = {
+    if (args.length != 1) {
+      throw new IllegalArgumentException("Exactly one parameter expected")
     }
+    val source = scala.io.Source.fromFile(args(0))
+    val contents = source.mkString
+    source.close()
+    
+    println(contents)
   }
+
+//  loop { line =>
+//    PicoParser(line) match {
+//      case PicoParser.Success(t, _) => println(eval(t))
+//      case e: PicoParser.NoSuccess => println(e)
+//    }
+//  }
 }
