34025eed85dcba3a448f6be8d5004031aa7a5c5d
Emily Blatter <eblatter@hmc.edu>
2014-10-04 13:48:59 -0700

    empty test files created

---
+++ src/test/scala/dsl/picobot/semantics/SemanticsTest.scala
@@ -0,0 +1,15 @@
+package picobot.semantics
+
+import org.scalatest._
+
+import dsl.picobot.ir._
+import dsl.picobot.parser._
+import dsl.picobot.semantics._
+import edu.hmc.langtools._
+
+class PicobotSemanticsTests extends FunSpec
+    with LangInterpretMatchers[AST, Int] {
+	//maybe helpful code stolen from external-lab
+  //override val parser = CalcParser.apply _
+  //override val interpreter = eval _
+}
