9e6b4df5d18f838483ba00de6da081a9b43929c0
Sisi Cheng <scheng@hmc.edu>
2014-10-04 12:49:16 -0700

    Re-added starter files.

---
+++ src/main/scala/dsl/picobot/calc.scala
@@ -0,0 +1,16 @@
+package dsl.picobot
+
+import scala.tools.nsc.EvalLoop
+import dsl.picobot.parser.PicoParser
+import dsl.picobot.semantics.eval
+
+object Calculator extends EvalLoop with App {
+  override def prompt = "> "
+
+  loop { line =>
+    PicoParser(line) match {
+      case PicoParser.Success(t, _) => println(eval(t))
+      case e: PicoParser.NoSuccess => println(e)
+    }
+  }
+}
