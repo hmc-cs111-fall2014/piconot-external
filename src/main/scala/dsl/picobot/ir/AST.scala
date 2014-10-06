a34f4c43b6177a0fde4a33a5f9bf11fced054fa5
Nick Carter <njc093@gmail.com>
2014-10-06 16:12:52 -0700

    Tests now pass again

--- src/main/scala/dsl/picobot/ir/AST.scala
+++ src/main/scala/dsl/picobot/ir/AST.scala
@@ -2,14 +2,16 @@ package dsl.picobot.ir
 
 sealed abstract class AST
 sealed abstract class Program extends AST
+
 sealed abstract class Rule extends Program
 
 
 case class Equal(lhs: Rule, rhs: Rule) extends Rule
-case class Lhs(state: State, surrounding: List[Surrounding]) extends Rule
+case class Lhs(state: State, surrounding: List[Rule]) extends Rule
 case class Rhs(state: State, nextDir: Dir) extends Rule
 
-class Surrounding extends Rule
+sealed abstract class Surrounding extends Rule
+// TODO: rename to blocked, open, any
 case class Plus(dir: Dir) extends Surrounding
 case class Minus(dir: Dir) extends Surrounding
 case class Mult(dir: Dir) extends Surrounding
--- src/test/scala/dsl/picobot/parser/ParserTest.scala
+++ src/test/scala/dsl/picobot/parser/ParserTest.scala
@@ -13,16 +13,35 @@ class PicoParserTests extends FunSpec with LangParseMatchers[AST] {
   describe("A program") {
 
     it("can be two states and an equal sign") {
-      program("1 = 2") should parseAs ( Equal(Lhs(State(1), None), Rhs(State(2), Stay())) )
+      program("1 = 2") should parseAs ( 
+          Equal(
+        		  	Lhs(
+        		  	    State(1), List.empty
+        		  	    ), 
+        		  	Rhs(
+        		  	    State(2), 
+        		  	    Stay()
+        		  	    )
+        		  ) 
+          )
     }
     
     it ("can have directions in lhs") {
-      program("1 + n = 2") should parseAs ( Equal(Lhs(State(1), Some(Plus(N(), None))), Rhs(State(2), Stay())) )
-      program("1 + n - w * s = 2") should parseAs ( Equal(Lhs(State(1), Some(Plus(N(), (Some(Minus(W(), (Some(Mult(S(), None))))))))), Rhs(State(2), Stay()) ) )
+      program("1 + n = 2") should parseAs ( 
+          Equal(
+              Lhs(State(1), List(
+                  Plus(N())
+                  )
+              ), Rhs(State(2), Stay())) )
+      program("1 + n - w * s = 2") should parseAs ( Equal(
+          Lhs(
+        		  State(1), 
+        		  List(Plus(N()), Minus(W()), Mult(S()))
+    		  ), Rhs(State(2), Stay()) ) )
     }
     
     it ("can have directions in rhs") {
-      program("2 = 2 - w") should parseAs ( Equal(Lhs(State(2), None), Rhs(State(2), W())))
+      program("2 = 2 - w") should parseAs ( Equal(Lhs(State(2), List.empty), Rhs(State(2), W())))
     }
 
   }
