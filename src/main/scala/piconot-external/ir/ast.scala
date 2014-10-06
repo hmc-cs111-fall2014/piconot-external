d131b6d2b027677cb9cf9a053e1dbd73664cf19e
Ellen Hui <ellenfkh@gmail.com>
2014-10-06 14:31:25 -0700

    set up preliminary IR

--- src/main/scala/piconot-external/ir/ast.scala
+++ src/main/scala/piconot-external/ir/ast.scala
@@ -1,1 +1,11 @@
-// TODO: Intermediate Representation goes here!
+sealed abstract class AST
+sealed abstract class expr extends AST
+
+case class News(stringDir:String) extends expr
+case class MoveDir(charDir:Char) extends expr
+case class MyState(stringState:String) extends expr
+
+case class InState(oldState:String, cases:expr*) extends expr // ???
+case class SurroundedBy(news:News) extends expr
+case class ThenMove(newDir: MoveDir) extends expr
+case class NewState(state: MyState) extends expr
