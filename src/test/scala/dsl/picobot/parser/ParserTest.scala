0d7085bb53a29fd35e3dba6c4e8ffd1135a89f60
Nick Carter <njc093@gmail.com>
2014-10-06 14:32:50 -0700

    Tests of parser now are typed properly

--- src/test/scala/dsl/picobot/parser/ParserTest.scala
+++ src/test/scala/dsl/picobot/parser/ParserTest.scala
@@ -13,16 +13,16 @@ class PicoParserTests extends FunSpec with LangParseMatchers[AST] {
   describe("A program") {
 
     it("can be two states and an equal sign") {
-      program("1 = 2") should parseAs ( Equal(Lhs(State(1), None), Rhs(State(2), None)) )
+      program("1 = 2") should parseAs ( Equal(Lhs(State(1), None), Rhs(State(2), Stay())) )
     }
     
     it ("can have directions in lhs") {
-      program("1 + n = 2") should parseAs ( Equal(Lhs(State(1), Plus(N(), None)),Rhs(2)) )
-      program("1 + n - w * s = 2") should parseAs ( Equal(Lhs(State(1), Plus(N(), (Minus(W(), (Mult(S(), None)))))), Rhs(State(2), None) ) )
+      program("1 + n = 2") should parseAs ( Equal(Lhs(State(1), Some(Plus(N(), None))), Rhs(State(2), Stay())) )
+      program("1 + n - w * s = 2") should parseAs ( Equal(Lhs(State(1), Some(Plus(N(), (Some(Minus(W(), (Some(Mult(S(), None))))))))), Rhs(State(2), Stay()) ) )
     }
     
     it ("can have directions in rhs") {
-      program("2 = 2 - w") should parseAs ( Equal(Lhs(State(2), None), Rhs(State(2), Minus(W(), None))))
+      program("2 = 2 - w") should parseAs ( Equal(Lhs(State(2), None), Rhs(State(2), W())))
     }
 
   }
