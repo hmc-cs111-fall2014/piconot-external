bd3f9cfeea6299e1ef9425abaa5ed7ce75d0e81f
Michael Culhane <mculhane11@gmail.com>
2014-10-06 13:54:37 -0700

    Added first stab at intermediate representation

---
+++ src/main/scala/piconot/ir/AST.scala
@@ -0,0 +1,21 @@
+package piconot.ir
+
+sealed abstract class AST
+
+case class Program(states: List[State]) extends AST
+case class State(number: StateNumber, rules: List[Rule]) extends AST
+case class Rule(freeDirections: List[CompassDirection],
+				blockedDirections: List[CompassDirection],
+				moveDirection: MoveDirection,
+				newState: StateNumber) extends AST
+case class StateNumber(number: Int) extends AST
+
+sealed abstract class MoveDirection extends AST
+sealed abstract class CompassDirection extends MoveDirection
+case object North extends CompassDirection
+case object South extends CompassDirection
+case object East extends CompassDirection
+case object West extends CompassDirection
+case object StayPut extends MoveDirection
+
+				
