b3ec8fb26e089a4321e96ac6319818c4dbbe4caf
Matthew Cook <mattcook@wl-24-138.cine.claremont.edu>
2014-10-08 14:11:18 -0700

    current working on parsing

--- src/main/scala/priorityBot/ir/AST.scala
+++ src/main/scala/priorityBot/ir/AST.scala
@@ -1,1 +1,31 @@
 package priorityBot.ir
+
+// PUT GRAMMAR HERE
+
+
+sealed abstract class AST
+sealed abstract class Program extends AST
+sealed abstract class Statement extends AST
+sealed abstract class CardinalDirection extends Statement
+sealed abstract class RelativeDirection extends Statement
+
+//case class CardinalDirection(direction: )
+
+case class Rule(cardinal: CardinalDirection,
+    dir1: RelativeDirection, 
+    dir2: RelativeDirection,
+    dir3: RelativeDirection,
+    dir4: RelativeDirection)
+    extends Statement
+    
+case class Maze(mazeName: String) extends Statement
+
+//
+//case class F extends RelativeDirection
+//case class B extends RelativeDirection
+//case class L extends RelativeDirection
+//case class R extends RelativeDirection
+//case class Front extends RelativeDirection
+//case class Back extends RelativeDirection
+//case class Left extends RelativeDirection
+//case class Right extends RelativeDirection
--- src/main/scala/priorityBot/parser/Parser.scala
+++ src/main/scala/priorityBot/parser/Parser.scala
@@ -1,1 +1,25 @@
 package priorityBot.parser
+
+import scala.util.parsing.combinator._
+import priorityBot.ir
+
+object BotParser extends JavaTokenParsers with PackratParsers {
+  
+  // parsing interface
+  def apply(s: String): ParseResult[AST] = parseAll(program, s)
+  
+//  def program: PackratParser[Program] = 
+//    (   "maze = "~>name~statement ^^ {case "maze = "~>mazeName => Maze(mazeName)}
+//        )
+//  def statement: PackratParser[Statement] = 
+//    (   rule<~"\n"~>rule<~"\n"~>rule<~"\n"~>rule ^^ 
+//    			{
+//      | anyRule ^^
+//        )
+//  def rule: PackratParser[Rule] = 
+//    (   rule<~"\n"~>rule<~"\n"~>rule<~"\n"~>rule ^^ 
+//    			{case "maze = "~>mazeName => Maze(mazeName)}
+//        )
+  
+  
+}
