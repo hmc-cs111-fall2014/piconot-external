3044c8a3838de856e0ae137cfd101d0a34fe21c5
Matthew Cook <mattcook@wl-31-42.cine.claremont.edu>
2014-10-09 21:40:29 -0700

    finished parsing code

--- src/main/scala/priorityBot/ir/AST.scala
+++ src/main/scala/priorityBot/ir/AST.scala
@@ -4,28 +4,20 @@ package priorityBot.ir
 
 
 sealed abstract class AST
-sealed abstract class Program extends AST
-sealed abstract class Statement extends AST
-sealed abstract class CardinalDirection extends Statement
-sealed abstract class RelativeDirection extends Statement
+
+case class Program(mazeName: String, rules: Rules) extends AST
 
 //case class CardinalDirection(direction: )
+
+case class Rules(rules: List[Rule]) extends AST
 
 case class Rule(cardinal: CardinalDirection,
     dir1: RelativeDirection, 
     dir2: RelativeDirection,
     dir3: RelativeDirection,
     dir4: RelativeDirection)
-    extends Statement
-    
-case class Maze(mazeName: String) extends Statement
+    extends AST
 
-//
-//case class F extends RelativeDirection
-//case class B extends RelativeDirection
-//case class L extends RelativeDirection
-//case class R extends RelativeDirection
-//case class Front extends RelativeDirection
-//case class Back extends RelativeDirection
-//case class Left extends RelativeDirection
-//case class Right extends RelativeDirection
+case class CardinalDirection(direction: String) extends AST
+
+case class RelativeDirection(direction: String) extends AST
--- src/main/scala/priorityBot/parser/Parser.scala
+++ src/main/scala/priorityBot/parser/Parser.scala
@@ -1,25 +1,34 @@
 package priorityBot.parser
 
 import scala.util.parsing.combinator._
-import priorityBot.ir
+import scala.util.matching.Regex
+import priorityBot.ir._
 
 object BotParser extends JavaTokenParsers with PackratParsers {
   
   // parsing interface
   def apply(s: String): ParseResult[AST] = parseAll(program, s)
   
-//  def program: PackratParser[Program] = 
-//    (   "maze = "~>name~statement ^^ {case "maze = "~>mazeName => Maze(mazeName)}
-//        )
-//  def statement: PackratParser[Statement] = 
-//    (   rule<~"\n"~>rule<~"\n"~>rule<~"\n"~>rule ^^ 
-//    			{
-//      | anyRule ^^
-//        )
-//  def rule: PackratParser[Rule] = 
-//    (   rule<~"\n"~>rule<~"\n"~>rule<~"\n"~>rule ^^ 
-//    			{case "maze = "~>mazeName => Maze(mazeName)}
-//        )
+  def program: Parser[Program] = 
+    (   "maze = " ~ "\\w*\\.txt".r~rules ^^ 
+        {case "maze = "~mazeName~ruleList => Program(mazeName, ruleList)}
+        )
+        
+        
+  def rules: Parser[Rules] = 
+    (   rule~repN(3, opt(rule)) ^^
+        {case rule~moreRules => Rules(List(rule) ++ moreRules.toList.flatten) }
+        )     
+        
+  def rule: Parser[Rule] = 
+    (   cardinalDirection~" => "~repN(4, relativeDirection) ^^
+        {case cardinal~" => "~dirs => Rule(cardinal, dirs(0), dirs(1), dirs(2), dirs(3))}
+        )
   
+  def cardinalDirection: Parser[CardinalDirection] =
+    "[NEWS\\*]".r ^^ {case c => CardinalDirection(c)}
+  
+  def relativeDirection: Parser[RelativeDirection] = 
+    "[FLBR]".r ^^ {case r => RelativeDirection(r)}
   
 }
