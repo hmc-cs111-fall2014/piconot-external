3d428846cec4b517bbca10511d73793408fa10b6
Paul Dapolito <pdapolito@hmc.edu>
2014-10-12 15:10:05 -0700

    Parser now considers whitespace

--- src/main/scala/piconot/parser/Parser.scala
+++ src/main/scala/piconot/parser/Parser.scala
@@ -4,16 +4,16 @@ import scala.util.parsing.combinator._
 import piconot.ir._
 
 object PiconotParser extends JavaTokenParsers with PackratParsers {
-
+	// tell parser to consider whitespace
+	override val skipWhitespace = false
+	
     // parsing interface
     def apply(s: String): ParseResult[AST] = parseAll(rule, s)
 
     // rules
     lazy val rule: PackratParser[PicobotProgram] = 
       (
-          state~surr ^^ {case state1~surr1 => Rule(state1,surr1)}
-          //state~surr~"->"~mov~state ^^  
-          //{case state1~surr1~"->"~movedir~state2 => Rule(state1, surr1, movedir, state2)}
+          state~" "~surr ^^ {case state1~" "~surr1 => Rule(state1,surr1)}
       )
                     
     // state
@@ -25,45 +25,19 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
     // surroundings
     lazy val surr: Parser[Surroundings] =  
     (
-      //pattern~pattern~pattern~pattern ^^ {case a~b~c~d => Surroundings(a, b, c, d)}
-      pattern2  ^^ {case a => Surroundings(a.charAt(0),a.charAt(1), a.charAt(2), a.charAt(3))}
-       
-      //starOrChar ^^ {a => Surroundings(a.charAt(0),a.charAt(1), a.charAt(2), a.charAt(3))}
+      surrComp~surrComp~surrComp~surrComp ^^ {case a~b~c~d => Surroundings(a, b, c, d)}
     )
-    
-    lazy val pattern2: Parser[String] = 
-      (
-          pattern~pattern~pattern~pattern ^^ {case a~b~c~d => a.toString + b.toString + c.toString + d.toString}
-      )
           
-    lazy val pattern: Parser[Char] = 
+    lazy val surrComp: Parser[Char] = 
       (
           free
           | wildcard
           | blocked
       )
       
-      
-      
-//     lazy val pattern2: Parser[String] = 
-//       (
-//         pattern~pattern~pattern~pattern ^^ 
-//         )
-      
      def free: Parser[Char] = 'x'
      def wildcard: Parser[Char] = '*'
      def blocked: Parser[Char] = 'N' 
-       
-//     lazy val blocked: Parser[Char] =
-//       ( north
-//         | South
-//         | West
-//         | East
-//           )
-//      
-//     def north: Parser[North] = 
-//        
-       
   
     // movement
     lazy val mov: Parser[MoveDirection] =
--- src/main/scala/piconot/semantics/Interpreter.scala
+++ src/main/scala/piconot/semantics/Interpreter.scala
@@ -4,10 +4,6 @@ import piconot.ir._
 
 package object semantics {
   def eval(ast: AST): PicobotProgram = ast match {
-    case Rule(startState: State, 
-    		  surroundings: Surroundings, 
-    		  moveDirection: MoveDirection, 
-    		  endState: State) ⇒ 
-    		  new Rule(startState, surroundings, moveDirection, endState)
+    case _ => null
   }
 }
