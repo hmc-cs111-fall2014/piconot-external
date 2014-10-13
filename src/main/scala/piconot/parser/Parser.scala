a6dcb002b8662c5d2661c72287a90f5bf5c25cac
Michael Culhane <mculhane11@gmail.com>
2014-10-13 16:09:55 -0700

    Added parser

---
+++ src/main/scala/piconot/parser/Parser.scala
@@ -0,0 +1,42 @@
+package piconot.parser
+
+import scala.util.parsing.combinator._
+import piconot.ir._
+
+object PiconotParser extends JavaTokenParsers with PackratParsers {
+
+    // parsing interface
+    def apply(s: String): ParseResult[AST] = parseAll(program, s)
+
+    // expressions
+    lazy val program: PackratParser[AST] =
+      rep1(state)~"RUN MAZE:"~"""[a-zA-Z0-9\._]+""".r ^^ {case states~_~mazeName => Program(states, mazeName)}
+    
+    lazy val state: PackratParser[State] = 
+      "STATE"~stateNumber~":"~rep1(rule) ^^ {case _~stateNum~_~rules => State(stateNum, rules)}
+    
+    lazy val rule: PackratParser[Rule] =
+      ( "FREE DIRECTIONS:"~repsep(surrounding, ",")~
+        "BLOCKED DIRECTIONS:"~repsep(surrounding, ",")~
+        "MOVE DIRECTION:"~moveDirection~
+        "NEW STATE:"~stateNumber ^^ {
+        case _~freeDirs~_~blockedDirs~_~moveDir~_~newState => Rule(freeDirs, blockedDirs, moveDir, newState)
+      })
+    
+    lazy val surrounding: PackratParser[Surrounding] = 
+      (   "N" ^^ {case _ => North}
+        | "E" ^^ {case _ => East}
+        | "W" ^^ {case _ => West}
+        | "S" ^^ {case _ => South})
+    
+    lazy val moveDirection: PackratParser[MoveDirection] = 
+      (   "N" ^^ {case _ => North}
+        | "E" ^^ {case _ => East}
+        | "W" ^^ {case _ => West}
+        | "S" ^^ {case _ => South}
+        | "X" ^^ {case _ => StayPut})
+      
+    // numbers
+    def stateNumber: Parser[StateNumber] = wholeNumber ^^ {s ⇒ StateNumber(s.toInt)}
+    
+ }
