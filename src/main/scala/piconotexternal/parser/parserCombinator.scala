cfa65e785205bc7e73031706bf72ceff01f6b44f
Ellen Hui <ellenfkh@gmail.com>
2014-10-13 15:57:39 -0700

    grammatically correct parse errors

--- src/main/scala/piconotexternal/parser/parserCombinator.scala
+++ src/main/scala/piconotexternal/parser/parserCombinator.scala
@@ -9,54 +9,54 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
   lazy val listRules:PackratParser[Expr] =
     (
       rep(inState) ^^ {case rules => RulesList(rules)}
-      | failure("listrules")
+      | failure("failed to parse a RulesList")
     )
 
   lazy val inState:PackratParser[InState] =
     (
       "in state"~state~rep(single) ^^ {case "in state"~state~singles => InState(state, singles)}
-      | failure("instate")
+      | failure("failed to parse an inState")
     )
 
   lazy val single:PackratParser[Single] =
     (
       surroundedBy~thenMove~newState ^^ {case news~dir~state => Single(news, dir, state)}
-      | failure("single")
+      | failure("failed to parse a Single")
     )
 
   lazy val surroundedBy: PackratParser[SurroundedBy]=
     (
       "surrounded by"~news ^^ {case "surrounded by"~news => SurroundedBy(news)}
-      | failure("surrounded by")
+      | failure("failed to parse a SurroundedBy")
     )
 
   lazy val thenMove: PackratParser[ThenMove]=
     (
       "then move"~dir ^^ {case "then move"~dir => ThenMove(dir)}
-      | failure("then move")
+      | failure("failed to parse a ThenMove")
     )
 
   lazy val newState: PackratParser[NewState]=
     (
       "next state"~state ^^ {case "next state"~newState => NewState(newState)}
-      | failure("next state")
+      | failure("failed to parse a NewState")
     )
 
   lazy val news: PackratParser[News] =
     (
       """[\\*\w]{4}""".r ^^ {News}
-      | failure("news")
+      | failure("failed to parse a News")
     )
 
   lazy val dir: PackratParser[MoveDir] =
     (
       """[NEWSX]""".r ^^ {case x => MoveDir(x.head)}
-      | failure("dir")
+      | failure("failed to parse a MoveDir")
     )
 
   lazy val state: PackratParser[MyState] =
     (
        """\w+""".r ^^ {MyState}
-      | failure("state")
+      | failure("failed to parse a MyState")
     )
 }
