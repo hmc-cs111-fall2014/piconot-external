eff6e00882f132a2362731fbfa891e943c406fed
Alex Gruver <agruver@g.hmc.edu>
2014-10-12 23:19:21 -0700

    rough draft of parser

---
+++ src/main/scala/piconot/parser/Parser.scala
@@ -0,0 +1,44 @@
+package piconot.parser
+
+import scala.util.parsing.combinator._
+import piconot.ir._
+
+object PiconotParser extends JavaTokenParsers with PackratParsers {
+
+    // parsing interface
+    def apply(s: String): ParseResult[AST] = parseAll(program, s)
+
+    // expressions
+    lazy val program: PackratParser[Expr] = 
+      (  state 
+      	| state~program ^^ {case state~program => Program(state, program)})
+   
+    //terms
+    lazy val state: PackratParser[Expr] =
+      number~"{"~ruleset~"}" ^^ {case num~"{"~rules~"}" => State(num,rules)}
+    // factors
+    lazy val ruleset: PackratParser[Expr] =
+      (rule 
+          | rule~ruleset ^^ {case rul~rs => RuleSet(rul,rs)})
+      
+    lazy val rule: PackratParser[Expr] = 
+      walls~"->"~direction~","~number ^^ {case w~"->"~d~","~n => Rule(w,d,n)}
+    
+    lazy val walls: PackratParser[Expr] = 
+     wall~wall~wall~wall ^^ {case w1~w2~w3~w4 => Walls(w1,w2,w3,w4)}
+
+    lazy val wall: PackratParser[Expr] =
+      ("*" ^^ {in => Wall(in)}
+      | "_" ^^ {in => Wall(in)}
+      | "X" ^^ {in => Wall(in)})
+      
+     lazy val direction: PackratParser[Expr] =
+       ("N" ^^ {in => Direction(in)}
+         | "E" ^^ {in => Direction(in)} 
+         | "W" ^^ {in => Direction(in)}
+         | "S" ^^ {in => Direction(in)})
+    
+    // numbers
+    def number: Parser[Num] = wholeNumber ^^ {s ⇒ Num(s.toInt)}
+    
+ }
