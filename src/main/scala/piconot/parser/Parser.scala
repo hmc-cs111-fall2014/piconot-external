a7563e8089b969ac9f0fe3e9668c7734086e80fa
Paul Dapolito <pdapolito@hmc.edu>
2014-10-13 22:34:17 -0700

    Added error checking

--- src/main/scala/piconot/parser/Parser.scala
+++ src/main/scala/piconot/parser/Parser.scala
@@ -30,31 +30,33 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
           state~" "~surr~" -> "~mov~" "~state ^^ 
           {case state1~" "~surr1~" -> "~movedir~" "~state2 => 
   			Rule(state1, surr1, movedir, state2)}
+          | failure("Invalid rule")
       )
                     
     // state
     lazy val state: Parser[State] = 
-     ( wholeNumber ^^ {s ⇒ State(s.toInt)} )
+     ( wholeNumber ^^ {s ⇒ State(s.toInt)} | failure("Invalid state"))
     
     // surroundings
     lazy val surr: Parser[Surroundings] =  
     (
-      surrCompNorth~surrCompEast~surrCompWest~surrCompSouth ^^ {case a~b~c~d => Surroundings(a, b, c, d)}
+      surrCompNorth~surrCompEast~surrCompWest~surrCompSouth ^^ {case a~b~c~d => Surroundings(a, b, c, d)} 
+      | failure("Invalid surroundings (bad order)")
     )
     
     // surrounding components (with proper ordering enforced)
     lazy val surrComp: Parser[SurroundingComponentType] = 
-      ( free | wildcard )
+      ( free | wildcard | failure("Invalid surrounding component") )
       
-    def free: Parser[SurroundingComponentType] = 'x' ^^^ Free | failure("Not valid")
-    def wildcard: Parser[SurroundingComponentType] = '*' ^^^ Wildcard | failure("Not valid")
+    def free: Parser[SurroundingComponentType] = 'x' ^^^ Free
+    def wildcard: Parser[SurroundingComponentType] = '*' ^^^ Wildcard
     
     lazy val surrCompNorth: Parser[SurroundingComponentType] = ( surrComp | north )
     lazy val surrCompEast: Parser[SurroundingComponentType] = ( surrComp | east )
     lazy val surrCompWest: Parser[SurroundingComponentType] = ( surrComp | west )
     lazy val surrCompSouth: Parser[SurroundingComponentType] = ( surrComp | south )
     
-    lazy val surrComps = ( north | south | east | west )
+    lazy val surrComps = ( north | south | east | west | failure("Invalid surrounding component"))
     def north: Parser[SurroundingComponentType] = 'N' ^^^ Blocked
     def south: Parser[SurroundingComponentType] = 'S' ^^^ Blocked
     def east: Parser[SurroundingComponentType] = 'E' ^^^ Blocked
@@ -66,13 +68,13 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
       movComp ^^ {a => MoveDirection(a)}
     )
     
-    lazy val moveDirs = ( movNor | movSouth | movEast | movWest )
+    lazy val moveDirs = ( movNor | movSouth | movEast | movWest | failure("Invalid move direction"))
     def movNor: Parser[MoveDirectionType] = 'N' ^^^ MoveNorth
     def movSouth: Parser[MoveDirectionType] = 'S' ^^^ MoveSouth
     def movEast: Parser[MoveDirectionType] = 'E' ^^^ MoveEast
     def movWest: Parser[MoveDirectionType] = 'W' ^^^ MoveWest
     def halt: Parser[MoveDirectionType] = 'X' ^^^ Halt
 
-    lazy val movComp: Parser[MoveDirectionType] = ( moveDirs | halt)
+    lazy val movComp: Parser[MoveDirectionType] = ( moveDirs | halt | failure("Invalid move direction"))
     
 }
