5d332f1f8e572ed46c57a2f97f51e3418e83297c
Nathan <nhall@hmc.edu>
2014-10-13 21:01:02 -0700

    Added simple errors for parser

--- src/main/scala/piconot/parser/Parser.scala
+++ src/main/scala/piconot/parser/Parser.scala
@@ -11,35 +11,43 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
     // expressions
     lazy val program: PackratParser[Expr] = 
       (state~program ^^ {case state~program => extProgram(state, program)}
-      |state )
+      |state
+      |error("Expected a piconot program."))
       	 
-   
     //terms
     lazy val state: PackratParser[Expr] =
-      number~"{"~ruleset~"}" ^^ {case num~"{"~rules~"}" => extState(num,rules)}
+      (number~"{"~ruleset~"}" ^^ {case num~"{"~rules~"}" => extState(num,rules)}
+      |error("Expected a state."))
     // factors
     lazy val ruleset: PackratParser[Expr] =
-      ( rule~ruleset ^^ {case rul~rs => extRuleSet(rul,rs)}
-       | rule)
+      (rule~ruleset ^^ {case rul~rs => extRuleSet(rul,rs)}
+      |rule
+      |error("Expected a non-empty set of rules."))
       
     lazy val rule: PackratParser[Expr] = 
-      walls~"->"~direction~","~number ^^ {case w~"->"~d~","~n => extRule(w,d,n)}
+      (walls~"->"~direction~","~number ^^ {case w~"->"~d~","~n => extRule(w,d,n)}
+      |error("Expected a rule."))
     
     lazy val walls: PackratParser[Expr] = 
-     wall~wall~wall~wall ^^ {case w1~w2~w3~w4 => extWalls(w1,w2,w3,w4)}
+     (wall~wall~wall~wall ^^ {case w1~w2~w3~w4 => extWalls(w1,w2,w3,w4)}
+     |error("Expected a walls definition."))
 
     lazy val wall: PackratParser[Expr] =
       ("*" ^^ {in => extWall(in)}
-      | "_" ^^ {in => extWall(in)}
-      | "X" ^^ {in => extWall(in)})
+      |"_" ^^ {in => extWall(in)}
+      |"X" ^^ {in => extWall(in)}
+      |error("Expected a wall character."))
       
      lazy val direction: PackratParser[Expr] =
        ("N" ^^ {in => extDirection(in)}
-         | "E" ^^ {in => extDirection(in)} 
-         | "W" ^^ {in => extDirection(in)}
-         | "S" ^^ {in => extDirection(in)})
+       |"E" ^^ {in => extDirection(in)} 
+       |"W" ^^ {in => extDirection(in)}
+       |"S" ^^ {in => extDirection(in)}
+       |error("Expected a direction."))
     
     // numbers
-    def number: Parser[extNum] = wholeNumber ^^ {s ⇒ extNum(s.toInt)}
+    def number: Parser[extNum] = 
+      (wholeNumber ^^ {s ⇒ extNum(s.toInt)}
+      |error("Expected a number"))
     
  }
