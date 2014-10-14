07686267150bf8b040e0cb1e44a411f43916701e
Nathan <nhall@hmc.edu>
2014-10-13 19:01:41 -0700

    Variable name changes

--- src/main/scala/piconot/semantics/Interpreter.scala
+++ src/main/scala/piconot/semantics/Interpreter.scala
@@ -5,11 +5,12 @@ import piconot.ir._
 package object semantics {
   def eval(ast: AST): Unit = ast match {
     case Num(i) ⇒ print(i)
-    case Program(left, right) ⇒ {eval(left); eval(right)}
+    case Program(first, rest) ⇒ {eval(first); eval(rest)}
     case State(number, ruleset) => {eval(number); eval(ruleset)}
-    case RuleSet(left, right) => {eval(left); eval(right)}
+    case RuleSet(first, rest) => {eval(first); eval(rest)}
+    // case Rule(w,d,n) => addRule(eval(w), eval(d), eval(n))
     case Rule(w,d,n) => {eval(w); eval(d); eval(n)}
-    case Walls(a,b,c,d) => {eval(a); eval(b); eval(c); eval(d)}
+    case Walls(n,e,w,s) => {eval(n); eval(e); eval(w); eval(s)}
     case Wall(expr) => print(expr)
     case Direction(expr) => print(expr)
   }
