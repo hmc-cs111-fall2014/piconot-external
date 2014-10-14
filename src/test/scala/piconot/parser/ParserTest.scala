bfae59bd91f63292216480a896551ab40801a31a
Nathan <nhall@hmc.edu>
2014-10-13 21:47:20 -0700

    We have a test. It's beautiful

--- src/test/scala/piconot/parser/ParserTest.scala
+++ src/test/scala/piconot/parser/ParserTest.scala
@@ -9,5 +9,24 @@ import edu.hmc.langtools._
 class ParserTest extends FunSpec with LangParseMatchers[AST] {
   
   override val parser = PiconotParser.apply _
+  
+  describe("A state") {
+
+    it("can have just one rule") {
+      program("0{**** -> N, 0}") should parseAs (  extState(
+    		  								       extNum(0),
+    		  								         extRule(
+    		  								           extWalls(extWall("*"),
+    		  								             extWall("*"),
+    		  								             extWall("*"),
+    		  								             extWall("*")
+    		  								           ),
+    		  								           extDirection("N"),
+    		  								           extNum(0)
+    		  								         )
+    		  								       ))
+    }
+
+  }
 
 }
