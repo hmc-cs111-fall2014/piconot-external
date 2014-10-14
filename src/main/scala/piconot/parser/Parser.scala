e7510468979bebaad5d74b19c681a65eeafbfeb0
Nathan <nhall@hmc.edu>
2014-10-13 21:22:50 -0700

    Changed error to failure

--- src/main/scala/piconot/parser/Parser.scala
+++ src/main/scala/piconot/parser/Parser.scala
@@ -12,42 +12,42 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
     lazy val program: PackratParser[Expr] = 
       (state~program ^^ {case state~program => extProgram(state, program)}
       |state
-      |error("Expected a piconot program."))
+      |failure("Expected a piconot program."))
       	 
     //terms
     lazy val state: PackratParser[Expr] =
       (number~"{"~ruleset~"}" ^^ {case num~"{"~rules~"}" => extState(num,rules)}
-      |error("Expected a state."))
+      |failure("Expected a state."))
     // factors
     lazy val ruleset: PackratParser[Expr] =
       (rule~ruleset ^^ {case rul~rs => extRuleSet(rul,rs)}
       |rule
-      |error("Expected a non-empty set of rules."))
+      |failure("Expected a non-empty set of rules."))
       
     lazy val rule: PackratParser[Expr] = 
       (walls~"->"~direction~","~number ^^ {case w~"->"~d~","~n => extRule(w,d,n)}
-      |error("Expected a rule."))
+      |failure("Expected a rule."))
     
     lazy val walls: PackratParser[Expr] = 
      (wall~wall~wall~wall ^^ {case w1~w2~w3~w4 => extWalls(w1,w2,w3,w4)}
-     |error("Expected a walls definition."))
+     |failure("Expected a walls definition."))
 
     lazy val wall: PackratParser[Expr] =
       ("*" ^^ {in => extWall(in)}
       |"_" ^^ {in => extWall(in)}
       |"X" ^^ {in => extWall(in)}
-      |error("Expected a wall character"))
+      |failure("Expected a wall character"))
       
      lazy val direction: PackratParser[Expr] =
        ("N" ^^ {in => extDirection(in)}
        |"E" ^^ {in => extDirection(in)} 
        |"W" ^^ {in => extDirection(in)}
        |"S" ^^ {in => extDirection(in)}
-       |error("Expected a direction."))
+       |failure("Expected a direction."))
     
     // numbers
     def number: Parser[extNum] = 
       (wholeNumber ^^ {s ⇒ extNum(s.toInt)}
-      |error("Expected a number"))
+      |failure("Expected a number"))
     
  }
--- src/test/scala/piconot/parser/ParserTest.scala
+++ src/test/scala/piconot/parser/ParserTest.scala
@@ -6,6 +6,8 @@ import piconot.ir._
 import piconot.parser._
 import edu.hmc.langtools._
 
-class ParserTest {
+class ParserTest extends FunSpec with LangParseMatchers[AST] {
+  
+  override val parser = PiconotParser.apply _
 
 }
