e85607e37386f3ec77eb92bbe6c04c7e6c942bbd
Michael Culhane <mculhane11@gmail.com>
2014-10-13 19:12:15 -0700

    Added error handling to parser

--- src/main/scala/piconot/parser/Parser.scala
+++ src/main/scala/piconot/parser/Parser.scala
@@ -10,33 +10,40 @@ object PiconotParser extends JavaTokenParsers with PackratParsers {
 
     // expressions
     lazy val program: PackratParser[Program] =
-      rep1(state)~"RUN MAZE:"~"""[a-zA-Z0-9\._]+""".r ^^ {case states~_~mazeName => Program(states, mazeName)}
+      (   rep1(state)~"RUN MAZE:"~"""[a-zA-Z0-9\._]+""".r ^^ {case states~_~mazeName => Program(states, mazeName)}
+        | failure("Unable to parse Program"))
     
     lazy val state: PackratParser[State] = 
-      "STATE"~stateNumber~":"~rep1(rule) ^^ {case _~stateNum~_~rules => State(stateNum, rules)}
+      ( "STATE"~stateNumber~":"~rep1(rule) ^^ {case _~stateNum~_~rules => State(stateNum, rules)}
+        | failure("Unable to parse State") )
     
     lazy val rule: PackratParser[Rule] =
       ( "FREE DIRECTIONS:"~repsep(surrounding, ",")~
         "BLOCKED DIRECTIONS:"~repsep(surrounding, ",")~
         "MOVE DIRECTION:"~moveDirection~
         "NEW STATE:"~stateNumber ^^ {
-        case _~freeDirs~_~blockedDirs~_~moveDir~_~newState => Rule(freeDirs, blockedDirs, moveDir, newState)
-      })
+        	case _~freeDirs~_~blockedDirs~_~moveDir~_~newState => Rule(freeDirs, blockedDirs, moveDir, newState)
+      	}
+        | failure("Unable to parse Rule") )
     
     lazy val surrounding: PackratParser[Surrounding] = 
       (   "N" ^^ {case _ => North}
         | "E" ^^ {case _ => East}
         | "W" ^^ {case _ => West}
-        | "S" ^^ {case _ => South})
+        | "S" ^^ {case _ => South}
+        | failure("Unable to parse Surrounding"))
     
     lazy val moveDirection: PackratParser[MoveDirection] = 
       (   "N" ^^ {case _ => North}
         | "E" ^^ {case _ => East}
         | "W" ^^ {case _ => West}
         | "S" ^^ {case _ => South}
-        | "X" ^^ {case _ => StayPut})
+        | "X" ^^ {case _ => StayPut}
+        | failure("Unable to parse MoveDirection"))
       
     // numbers
-    def stateNumber: Parser[StateNumber] = wholeNumber ^^ {s ⇒ StateNumber(s.toInt)}
+    def stateNumber: Parser[StateNumber] =
+      (   wholeNumber ^^ {s ⇒ StateNumber(s.toInt)}
+        | failure("Unable to parse stateNumber"))
     
  }
